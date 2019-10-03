b12b4219cf58c09ec0148a2c2ca14033d47d0a40
Abelinho <agbachiabel@gmail.com>
2019-10-03 08:03:37 +0100

    This is the read file

--- src/web/jdbc/StudentControllerServlet.java
+++ src/web/jdbc/StudentControllerServlet.java
@@ -66,7 +66,11 @@ public class StudentControllerServlet extends HttpServlet {
 			case "UPDATE":
 				updateStudent(request, response);
 				break;
-							
+			
+			case "DELETE":
+				deleteStudent(request, response);
+				break;
+				
 			default:
 				listStudents(request, response);
 			}
@@ -76,6 +80,19 @@ public class StudentControllerServlet extends HttpServlet {
 			throw new ServletException(exc);
 		}
 		
+	}
+
+	private void deleteStudent(HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		// read student id from form data
+		String theStudentId = request.getParameter("studentId");
+		
+		// delete student from database
+		studentDbUtil.deleteStudent(theStudentId);
+		
+		// send them back to "list students" page
+		listStudents(request, response);
 	}
 
 	private void updateStudent(HttpServletRequest request, HttpServletResponse response)
